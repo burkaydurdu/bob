737f54b670703b37f7a2de07e9c2e95a4bb3ceb5
Burkay Durdu <burkaydurdu@outlook.com>
2019-12-02 11:23:42 +0300

    add site

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,10 @@ module Bob
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => 'https://pisano-bob.netlify.com',
+      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    }
   end
 end
