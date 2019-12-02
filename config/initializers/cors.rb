ac619d2b4a2d33098c1ad9e415be797f0bd68955
Burkay Durdu <burkaydurdu@outlook.com>
2019-12-02 11:31:14 +0300

    add site

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:4200'
+    origins '*'
     resource '*',
       headers: :any,
       methods: %i(get post put patch delete options head)
